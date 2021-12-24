ad1e2ab9a496b58dbcfb679e0060a2642d63ad11
cyishere <cyishere@gmail.com>
2021-12-24 11:41:13 +0800

    Add keywords on Map

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,13 +1,16 @@
 import { Dialog } from "@reach/dialog";
 import Image from "next/image";
+import Link from "next/link";
 
-import { container, close } from "./Map.module.css";
+import styles from "./Map.module.css";
 import UnstyledButton from "../UnstyledButton";
+import VisuallyHidden from "../VisuallyHidden";
+import { KEYWORDS, LOCATION } from "@/data/selectors";
 
 function Map({ isOpen, handleClose }) {
   return (
-    <Dialog isOpen={isOpen} className={container}>
-      <div className={close}>
+    <Dialog isOpen={isOpen} className={styles.container}>
+      <div className={styles.close}>
         <UnstyledButton onClick={handleClose}>
           <Image
             src="/images/close-icon_2x.png"
@@ -18,6 +21,93 @@ function Map({ isOpen, handleClose }) {
             priority="true"
           />
         </UnstyledButton>
+      </div>
+
+      <div className={styles.grid}>
+        <div className={styles.locationColumn}>
+          <h2>
+            <VisuallyHidden>By Location</VisuallyHidden>
+            <Image
+              src="/images/by-location.png"
+              alt=""
+              layout="fixed"
+              width={230}
+              height={68}
+              property="true"
+            />
+          </h2>
+          <div className={styles.world}>
+            {LOCATION.map((place) => (
+              <div className={styles.location} key={place.id}>
+                <VisuallyHidden>{place.text}</VisuallyHidden>
+                <Link href={`/location/${place.text.toLowerCase()}`} passHref>
+                  <a>
+                    <Image
+                      src={place.image}
+                      alt=""
+                      layout="fixed"
+                      width={place.width}
+                      height={place.height}
+                      property="true"
+                    />
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+        <aside className={styles.keywordsColumn}>
+          <h2>
+            <VisuallyHidden>By Keywords</VisuallyHidden>
+            <Image
+              src="/images/by-keywords.png"
+              alt=""
+              layout="fixed"
+              width={240}
+              height={65}
+              property="true"
+            />
+          </h2>
+
+          <div className={styles.keywords}>
+            {KEYWORDS.map((keyword) => (
+              <div
+                key={keyword.id}
+                className={styles.keyword}
+                style={{
+                  "--width": keyword.width + "px",
+                  "--height": keyword.height + "px",
+                }}
+              >
+                <VisuallyHidden>{keyword.text}</VisuallyHidden>
+                <Link href={`/keywords/${keyword.text.toLowerCase()}`} passHref>
+                  <a className={styles.content}>
+                    <div className={styles.wrapper}>
+                      <Image
+                        src={keyword.images.unselected}
+                        alt=""
+                        layout="fixed"
+                        width={keyword.width}
+                        height={keyword.height}
+                        property="true"
+                      />
+                    </div>
+                    <div className={styles.wrapper}>
+                      <Image
+                        src={keyword.images.selected}
+                        alt=""
+                        layout="fixed"
+                        width={keyword.width}
+                        height={keyword.height}
+                        property="true"
+                      />
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </aside>
       </div>
     </Dialog>
   );
---
+++ data/selectors.js
@@ -0,0 +1,188 @@
+/**
+ * @typedef {Object} KeywordImagesType
+ * @property {string} selected - The image of the selected state.
+ * @property {string} unselected - The image of the unselected state.
+ */
+
+/**
+ * @typedef {Object} KeywordType
+ * @property {number} id
+ * @property {string} text - The keyword in text
+ * @property {KeywordImagesType} images - The image's url of this keyword
+ * @property {number} width
+ * @property {number} height
+ */
+
+/**
+ * @type {KeywordType[]} KEYWORDS
+ */
+export const KEYWORDS = [
+  {
+    id: 1,
+    text: "Dreams",
+    images: {
+      selected: "/images/keywords/dreams-selected.png",
+      unselected: "/images/keywords/dreams-unselected.png",
+    },
+    width: 180,
+    height: 60,
+  },
+  {
+    id: 2,
+    text: "Travel",
+    images: {
+      selected: "/images/keywords/travel-selected.png",
+      unselected: "/images/keywords/travel-unselected.png",
+    },
+    width: 183,
+    height: 60,
+  },
+  {
+    id: 3,
+    text: "Other' Stories",
+    images: {
+      selected: "/images/keywords/others-stories-selected.png",
+      unselected: "/images/keywords/others-stories-unselected.png",
+    },
+    width: 286,
+    height: 66,
+  },
+  {
+    id: 4,
+    text: "Coffee",
+    images: {
+      selected: "/images/keywords/coffee-selected.png",
+      unselected: "/images/keywords/coffee-unselected.png",
+    },
+    width: 177,
+    height: 60,
+  },
+  {
+    id: 5,
+    text: "Fiction",
+    images: {
+      selected: "/images/keywords/fiction-selected.png",
+      unselected: "/images/keywords/fiction-unselected.png",
+    },
+    width: 190,
+    height: 63,
+  },
+  {
+    id: 6,
+    text: "Life in Japan",
+    images: {
+      selected: "/images/keywords/life-in-japan-selected.png",
+      unselected: "/images/keywords/life-in-japan-unselected.png",
+    },
+    width: 306,
+    height: 67,
+  },
+  {
+    id: 7,
+    text: "Infographic",
+    images: {
+      selected: "/images/keywords/infographic-selected.png",
+      unselected: "/images/keywords/infographic-unselected.png",
+    },
+    width: 262,
+    height: 66,
+  },
+  {
+    id: 8,
+    text: "Everyday Life",
+    images: {
+      selected: "/images/keywords/everyday-life-selected.png",
+      unselected: "/images/keywords/everyday-life-unselected.png",
+    },
+    width: 305,
+    height: 67,
+  },
+  {
+    id: 9,
+    text: "Readings",
+    images: {
+      selected: "/images/keywords/readings-selected.png",
+      unselected: "/images/keywords/readings-unselected.png",
+    },
+    width: 208,
+    height: 62,
+  },
+];
+
+/**
+ * @typedef {Object} LocationType
+ * @property {number} id
+ * @property {string} text - The keyword in text
+ * @property {string} image - The image's url of this keyword
+ * @property {number} width
+ * @property {number} height
+ */
+
+/**
+ * @type {LocationType[]} LOCATION
+ */
+export const LOCATION = [
+  {
+    id: 1,
+    text: "Japan",
+    image: "/images/location/japan.png",
+    width: 102,
+    height: 63,
+  },
+  {
+    id: 2,
+    text: "Cambodia",
+    image: "/images/location/cambodia.png",
+    width: 147,
+    height: 65,
+  },
+  {
+    id: 3,
+    text: "China",
+    image: "/images/location/china.png",
+    width: 93,
+    height: 60,
+  },
+  {
+    id: 4,
+    text: "Laos",
+    image: "/images/location/laos.png",
+    width: 84,
+    height: 53,
+  },
+  {
+    id: 5,
+    text: "UAE",
+    image: "/images/location/uae.png",
+    width: 84,
+    height: 54,
+  },
+  {
+    id: 6,
+    text: "Morocco",
+    image: "/images/location/morocco.png",
+    width: 153,
+    height: 58,
+  },
+  {
+    id: 7,
+    text: "Canada",
+    image: "/images/location/canada.png",
+    width: 112,
+    height: 58,
+  },
+  {
+    id: 8,
+    text: "Cuba",
+    image: "/images/location/cuba.png",
+    width: 98,
+    height: 61,
+  },
+  {
+    id: 9,
+    text: "Colombia",
+    image: "/images/location/colombia.png",
+    width: 146,
+    height: 58,
+  },
+];
